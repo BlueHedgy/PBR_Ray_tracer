655df8e840c882f2f0b30546f047a09e01bf7e49
hedgy <hedgybleu@outlook.com>
2025-12-14 13:49:57 +0100

    misc: added shlick's approximation for dieletric material

--- material.h
+++ material.h
@@ -61,9 +61,18 @@ class dielectric: public material {
         double ri = rec.front_face ? (1.0/refraction_index) : refraction_index;
 
         vec3 unit_direction = unit_vector(r_in.direction());
-        vec3 refracted = refract(unit_direction, rec.normal, ri);
 
-        scattered = ray(rec.point_incident, refracted);
+        double cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
+        double sin_theta = std::sqrt(1.0 - cos_theta*cos_theta);
+
+        bool cannot_refract = ri * sin_theta > 1.0; // if true, MUST reflect
+        vec3 reflect_direction;
+        if (cannot_refract || reflectance(cos_theta, ri) > random_double())
+            reflect_direction = reflect(unit_direction, rec.normal);
+        else
+            reflect_direction = refract(unit_direction, rec.normal, ri);
+
+        scattered = ray(rec.point_incident, reflect_direction);
         return true;
     }
 
@@ -71,6 +80,18 @@ class dielectric: public material {
     // Refractive index in vacuum or air, or the ratio of the material's refractive index over
     // the refractive index of the enclosing media
     double refraction_index;
+
+    static double reflectance(double cosine, double refraction_index){
+      // Shlick's approximation for reflectance
+      auto r0 = (1 - refraction_index) / (1 + refraction_index);
+      r0 = r0 * r0;
+      return r0 + (1-r0) * std::pow((1 - cosine), 5);
+    }
+};
+
+class meta_material : public material {
+  meta_material(){}
+  meta_material(){}
 };
 
 #endif
