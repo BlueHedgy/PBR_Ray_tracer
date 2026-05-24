eca3f933807bc8b0cd497c84b085117633e8e87d
hedgy <hedgybleu@outlook.com>
2026-05-24 11:35:35 +0200

    img: extra testing textures

--- material/material.h
+++ material/material.h
@@ -4,6 +4,7 @@
 #include "hittable.h"
 #include "texture.h"
 #include "aabb.h"
+#include <variant>
 
 /// @brief Abstract material class
 class material {
@@ -48,22 +49,29 @@ class material {
       return 0.5 * pow((g-c)/(g+c), 2) * (1 + pow( (c*(g+c) - 1) / (c*(g-c) +1), 2));
     }
 
-    vec3 schlick_approx_dielectric(double &eta1, double &eta2, const vec3 &view_direction, const vec3 &half_vector) const {
-      double fzero = fresnel_0_dielectric(eta1, eta2);
+    vec3 mix (const vec3& x, const vec3& y, const vec3& a) const {
+      return x * (vec3(1, 1, 1) - a) + y * a;
+    }
+
+    vec3 schlick_approx_dielectric(const color& albedo, const double& metalness, double &eta1, double &eta2, const vec3 &view_direction, const vec3 &half_vector) const {
+      // double fzero = fresnel_0_dielectric(eta1, eta2);
+      double fzero = 0.4;
       vec3 f0 = vec3(fzero, fzero, fzero);
+      vec3 metal = vec3(metalness, metalness, metalness);
+      f0 = mix(f0, albedo, metal);
       return f0 + (vec3(1,1,1) - f0) * pow((1.0 - dot(view_direction, half_vector)), 5);
     }
 
   protected:
 
-    double GGX_distribution(double &roughness, const vec3& normal, const vec3& half_vector) const {
+    double GGX_distribution(const double &roughness, const vec3& normal, const vec3& half_vector) const {
       double alpha = roughness*roughness;
       double dot_n_h = dot(normal, half_vector);
       double x = dot_n_h > 0 ? 1: 0;
       return x * (alpha*alpha) / (Pi * pow( pow(dot_n_h, 2) * (alpha*alpha - 1) + 1, 2));
     }
 
-    double Blinn_distribution(double &roughness, const vec3& normal, const vec3& half_vector) const {
+    double Blinn_distribution(const double &roughness, const vec3& normal, const vec3& half_vector) const {
       double alpha = roughness*roughness;
       double ns = 2/(alpha*alpha) - 2;
       double dot_h_n = dot(half_vector, normal);
@@ -86,13 +94,13 @@ class material {
       return std::min(1.00, std::min(G1, G2));
     }
 
-    auto Cook_Torrance_Microfacet_BRDF(double roughness, double refraction_index, const vec3& normal, const vec3& view_direction, vec3& light_direction) const {
+    auto Cook_Torrance_Microfacet_BRDF(const color& albedo, const double& metalness, const double& roughness, double refraction_index, const vec3& normal, const vec3& view_direction, vec3& light_direction) const {
       double air_index = 1;
       vec3 half_vector = unit_vector(light_direction + view_direction);
       // double D = Blinn_distribution(roughness, normal, half_vector);
       auto D = GGX_distribution(roughness, normal, half_vector);
       double G = Cook_Torrance_Geo_term(normal, view_direction, light_direction, half_vector);
-      vec3 F = schlick_approx_dielectric(refraction_index, air_index, normal, half_vector);
+      vec3 F = schlick_approx_dielectric(albedo, metalness, refraction_index, air_index, normal, half_vector);
 
 
       return D*G*F / (4 * dot(normal, light_direction) * dot(normal, view_direction));
@@ -104,50 +112,18 @@ class pbr_material : public material {
     double diffuse_coeff;
     point3 light_pos;
 
-    pbr_material (
-      const color& albedo,
-      const double& roughness,
-      const double& diffuse_coeff,
-      const point3& light_pos)
-      :
-      albedo(std::make_shared<solid_color>(albedo)),
-      roughness(std::make_shared<solid_color>(roughness)),
-      diffuse_coeff(diffuse_coeff),
-      light_pos(light_pos)
-    {}
+    typedef std::variant<color, std::shared_ptr<texture>> texture_map;
 
     pbr_material (
-      const std::shared_ptr<texture> &albedo,
-      const std::shared_ptr<texture> &roughness,
+      const texture_map& albedo,
+      const texture_map& metalness,
+      const texture_map& roughness,
       const double& diffuse_coeff,
       const point3& light_pos)
       :
-      albedo(albedo),
-      roughness(roughness),
-      diffuse_coeff(diffuse_coeff),
-      light_pos(light_pos)
-    {}
-
-    pbr_material (
-      const std::shared_ptr<texture> &albedo,
-      const double& roughness,
-      const double& diffuse_coeff,
-      const point3& light_pos)
-      :
-      albedo(albedo),
-      roughness(std::make_shared<solid_color>(roughness)),
-      diffuse_coeff(diffuse_coeff),
-      light_pos(light_pos)
-    {}
-
-    pbr_material (
-      const color& albedo,
-      const std::shared_ptr<texture> &roughness,
-      const double& diffuse_coeff,
-      const point3& light_pos)
-      :
-      albedo(std::make_shared<solid_color>(albedo)),
-      roughness(roughness),
+      albedo(process_input_texture(albedo)),
+      metalness(process_input_texture(metalness)),
+      roughness(process_input_texture(roughness)),
       diffuse_coeff(diffuse_coeff),
       light_pos(light_pos)
     {}
@@ -155,8 +131,9 @@ class pbr_material : public material {
     color pbr_color(const ray& ray_in, const hit_record& rec, const color& light_color, const point3& light_pos) const override {
       color albedo_value = albedo->value(rec.u, rec.v, rec.point_incident);
       double roughness_value = roughness->value(rec.u, rec.v, rec.point_incident)[0];
+      double metal_value = metalness->value(rec.u, rec.v, rec.point_incident)[0];
       vec3 light_dir = unit_vector(light_pos - rec.point_incident);
-      auto specular = Cook_Torrance_Microfacet_BRDF(roughness_value, 0.58, rec.normal, -ray_in.direction(), light_dir);
+      auto specular = Cook_Torrance_Microfacet_BRDF(albedo_value, metal_value, roughness_value, 0.58, rec.normal, -ray_in.direction(), light_dir);
 
       color pbr =  (diffuse_coeff * albedo_value / Pi + specular * (1.00-diffuse_coeff)) *
                     light_color * dot(rec.normal, light_dir);
@@ -176,7 +153,18 @@ class pbr_material : public material {
 
   private:
     std::shared_ptr<texture> albedo;
+    std::shared_ptr<texture> metalness;
     std::shared_ptr<texture> roughness;
+
+
+    std::shared_ptr<texture> process_input_texture (const texture_map& map_in) const {
+        if (std::holds_alternative<color>(map_in)){
+          return std::make_shared<solid_color>(std::get<color>(map_in));
+        }
+        else if (std::holds_alternative<std::shared_ptr<texture>>(map_in)) {
+          return std::get<std::shared_ptr<texture>>(map_in);
+        }
+    }
 
 };
 
